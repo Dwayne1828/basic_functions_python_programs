50c43f9fd6887f1e3c2adc89c1daef4a1eb3df5a
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 23:02:50 +0800

    print product

--- product_two_numbers.py
+++ product_two_numbers.py
@@ -3,3 +3,9 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
+#Calculates the product of the two numbers
+product = num1 * num2
+
+#Prints the product of the two numbers
+print(product)
+
