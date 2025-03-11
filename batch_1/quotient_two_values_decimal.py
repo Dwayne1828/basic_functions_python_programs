99dbb573d3f9dd8a66e0e6f438380679358e2d11
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 00:04:42 +0800

    rename variable quotient_decimal

--- batch_1/quotient_two_values_decimal.py
+++ batch_1/quotient_two_values_decimal.py
@@ -4,7 +4,7 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 #Calculates the quotient of the two numbers
-quotient = num1 / num2
+quotient_decimal = num1 / num2
 
 #Prints the quotient with decimal of the two numbers
-print(quotient)
+print(quotient_decimal)
