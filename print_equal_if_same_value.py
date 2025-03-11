4b1c9017dfd1f2cae01fa3a35906571b710fb69f
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 22:42:55 +0800

    input values and comments

---
+++ print_equal_if_same_value.py
@@ -0,0 +1,5 @@
+#Asks the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#Then compares the two numbers and prints the bigger number
