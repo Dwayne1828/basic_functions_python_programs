e95b88f51d502ac89de6c21f1eb6a8e89d679fff
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 22:44:07 +0800

    compares if equal

--- print_equal_if_same_value.py
+++ print_equal_if_same_value.py
@@ -3,3 +3,5 @@ num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
 #Then compares the two numbers and prints the bigger number
+if num1 == num2:
+    print("Equal")
