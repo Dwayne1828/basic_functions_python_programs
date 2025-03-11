7e9ded39893ee799cde68a8eae77c8bf9293562e
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 22:33:44 +0800

    prints bigger value

--- print_the_bigger_number.py
+++ print_the_bigger_number.py
@@ -2,3 +2,9 @@
 #Asks the user for two numbers 
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
+
+#Compares the two numbers and prints the bigger number
+if num1 > num2:
+    print(num1)
+elif num2 > num1:
+    print(num2)
