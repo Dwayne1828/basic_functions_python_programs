9dd4f34300f357eca421dbd404d07582dd77dfed
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 23:11:42 +0800

    print quotient with decimal

--- quotient_two_values_decimal.py
+++ quotient_two_values_decimal.py
@@ -3,3 +3,8 @@
 num1 = int(input("Enter the first number: "))
 num2 = int(input("Enter the second number: "))
 
+#Calculates the quotient of the two numbers
+quotient = num1 / num2
+
+#Prints the quotient with decimal of the two numbers
+print(quotient)
