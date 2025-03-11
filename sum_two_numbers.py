91fa929a42f69a2c55bf6a92baa03b3ff45fc90f
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 22:48:52 +0800

    sum of two values

---
+++ sum_two_numbers.py
@@ -0,0 +1,7 @@
+
+#Asks for two numbers from the user
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+#Calculates the sum of the two numbers
+sum = num1 + num2
