9df451b7e0cc1db4049968148eef50342c92c1e2
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 23:31:30 +0800

    counts odd and print

--- count_odd_inputs.py
+++ count_odd_inputs.py
@@ -4,3 +4,9 @@ odd = 0
 #Asks the user for values ten times
 for i in range(10): 
     num = int(input("Enter a number: "))
+    #If the number is odd, increment odd by 1
+    if num % 2 != 0:
+        odd += 1
+
+#Prints the total number of odd numbers
+print(odd)
