855399167bfcc2336f14ca00111ada5db6dc1213
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-11 23:26:00 +0800

    total sum

--- sum_of_ten_numbers.py
+++ sum_of_ten_numbers.py
@@ -5,4 +5,8 @@ sum = 0
 #Asks the user for values ten times
 for i in range(10):
     num = int(input("Enter a number: "))
-    
+    #Adds the value to the total sum
+    sum += num
+
+#Prints the total sum
+print(sum)
