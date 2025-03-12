b9b2917ad2eebdba255577abfca58dd92d0cab93
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:20:01 +0800

    prints quotient without deci

--- quotient_without_decimal.py
+++ quotient_without_decimal.py
@@ -1,9 +1,13 @@
 
 #Asks the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
-#Solve for the quotient using floor division and if float convert to int
+#Solve for the quotient using floor division 
+quotient_wihout_decimal = num1 // num2
 
 #Print the quotient of the two numbers
+print(quotient_wihout_decimal)
 
 
 
