418420eb58c451857ddba1d6e4a8a0b05bfcd575
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 23:48:54 +0800

    count dictionary in most dupli funct

--- batch_4/till_invalid_display_most_dupli.py
+++ batch_4/till_invalid_display_most_dupli.py
@@ -10,3 +10,14 @@ while True:
     except ValueError:
         break
 
+def most_duplicate(list_of_inputs):
+    
+    count_dict = {}
+    
+    for number in list_of_inputs:
+        count_dict[number] = list_of_inputs.count(number)
+    
+    return count_dict
+
+count = (most_duplicate(list_of_inputs))
+print(count)   
