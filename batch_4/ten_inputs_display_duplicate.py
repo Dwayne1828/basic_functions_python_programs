7f6b4b13092a20fabe3a68baed072709756d7814
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 23:01:36 +0800

    10 inputs in a list

--- batch_4/ten_inputs_display_duplicate.py
+++ batch_4/ten_inputs_display_duplicate.py
@@ -1,2 +1,10 @@
 
+#Create a list to store 10 inputs
+list_of_inputs = [] 
+
+#Create a loop to get 10 inputs from the user
+for i in range(10):
+    input_value = input("Enter a value: ")
+    list_of_inputs.append(input_value)
+
 
