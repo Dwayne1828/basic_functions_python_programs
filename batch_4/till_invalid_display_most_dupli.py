265083320e7926ae2ade29c6cc9ae4244763a53e
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-13 00:05:30 +0800

    finds max duplicate and prints

--- batch_4/till_invalid_display_most_dupli.py
+++ batch_4/till_invalid_display_most_dupli.py
@@ -11,13 +11,14 @@ while True:
         break
 
 def most_duplicate(list_of_inputs):
-    
-    count_dict = {}
-    
+    count_dict = {} #Create a dictionary to store the count of each number
     for number in list_of_inputs:
         count_dict[number] = list_of_inputs.count(number)
     
-    return count_dict
+    #Find the number with the highest count in the dictionary items
+    for key, value in count_dict.items():
+        if value == max(count_dict.values()):
+            return key, value 
 
-count = (most_duplicate(list_of_inputs))
-print(count)   
+max_duplicate, count = most_duplicate(list_of_inputs)
+print("Most duplicate: ", max_duplicate, "Count: ", count)
