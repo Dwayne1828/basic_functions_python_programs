bfc17c634fda90a8a0b84a0d08fa3eb3cdd58e3a
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:43:57 +0800

    checks even count and print

--- even_counter_ten_inputs.py
+++ even_counter_ten_inputs.py
@@ -5,4 +5,9 @@ even_counter = 0
 
 for i in range(10):
     num = int(input("Enter a number: ")) 
-    
+    #Checks if the number is even
+    if num % 2 == 0:
+        even_counter += 1 #Iterate even_counter by 1  
+
+#Print the number of even numbers
+print(even_counter)
