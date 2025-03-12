916309b308c4d284f3d15e96563780e26f9c5619
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 17:08:20 +0800

    pseudocode between number file

---
+++ prints_between_input_two_numbers.py
@@ -0,0 +1,7 @@
+
+#Asks the user for two numbers
+
+#Check whose number is the smallest and biggest 
+
+#Using for loop that sets the range to what is inputted by the user 
+#Prints the numbers between the two numbers
