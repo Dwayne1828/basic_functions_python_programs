ff4f8c8715d9f584cc2220afbaec38b540877f4e
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 19:02:34 +0800

    once if dupli inputs

---
+++ display_all_numbers_once_if_dupli.py
@@ -0,0 +1,9 @@
+
+#Asks the user for 10 numbers
+#Creates an empty list to store the numbers
+input_list = [] 
+
+for i in range(10):
+    number = int(input("Enter a number: "))
+    input_list.append(number) #Adds the number to the list
+    
