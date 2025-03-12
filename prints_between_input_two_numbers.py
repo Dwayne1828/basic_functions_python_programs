fec05a43a8bd6b9e6b3c6bc63486ed72b7047764
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 17:13:13 +0800

    between file done

--- prints_between_input_two_numbers.py
+++ prints_between_input_two_numbers.py
@@ -9,3 +9,7 @@ if num1 < num2:
     for i in range(num1 + 1, num2):
         print(i) #Prints the numbers between the two numbers
 
+if num2 < num1:
+    for i in range(num2 + 1, num1):
+        print(i)
+
