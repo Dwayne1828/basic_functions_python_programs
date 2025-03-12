099af2c89a8cd7a94c7d4f4d381e7c80c5fe0900
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-13 00:22:39 +0800

    while loop till invalid

---
+++ batch_4/till_invalid_average.py
@@ -0,0 +1,13 @@
+
+#Create a list to store the inputs
+list_of_inputs = []
+
+#Asks the user  till they enter an invalid input
+while True:
+    try:
+        number = int(input("Enter a value: "))
+        list_of_inputs.append(number)
+    except ValueError:
+        break
+
+    
