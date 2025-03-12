3022f2efc51a7c6b4dfdbe1aeec2e86d99b859c1
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-13 00:26:51 +0800

    average of the list till invalid

--- batch_4/till_invalid_average.py
+++ batch_4/till_invalid_average.py
@@ -10,4 +10,10 @@ while True:
     except ValueError:
         break
 
-    
+def average(list_of_inputs):
+    #Calculate the average of the numbers
+    average = sum(list_of_inputs) / len(list_of_inputs)
+    return average
+
+average_value = average(list_of_inputs)
+print("Average: ", average_value)
