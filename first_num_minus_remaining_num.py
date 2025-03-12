301b66369e8704540a505305746f5cd5a0a89998
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:36:46 +0800

    solve for difference and initialized sum_remaining

--- first_num_minus_remaining_num.py
+++ first_num_minus_remaining_num.py
@@ -1,8 +1,10 @@
 
+sum_remaining = 0
+
 #Asks the user for 10 numbers using a loop
 for i in range(10): 
     num = int(input("Enter a number: "))
-    
+
 #Store the first number in a variable and the sum of remaining 
     if i == 0:
         first_num = num
@@ -10,7 +12,8 @@ for i in range(10):
         sum_remaining += num
 
 #Solve for the difference of the first number and the sum of the remaining numbers
+difference = first_num - sum_remaining
 
 #Print the difference 
-
+print(difference)
     
