db50238714227e82600826d5ee0f98390b139da5
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-13 00:17:50 +0800

    sort highest to lowest

--- till_invalid_highest_to_lowest.py
+++ till_invalid_highest_to_lowest.py
@@ -9,3 +9,11 @@ while True:
         list_of_inputs.append(number)
     except ValueError:
         break
+
+#Create a function to sort the numbers from highest to lowest
+def highest_to_lowest(list_of_inputs):
+    list_of_inputs.sort(reverse=True)
+    return list_of_inputs
+
+high_to_low = highest_to_lowest(list_of_inputs)
+print("Highest to lowest: ", high_to_low)
