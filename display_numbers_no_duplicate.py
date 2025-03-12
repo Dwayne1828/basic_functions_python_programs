0fd4f973064e4c446f33c6cd34bb5e9f8e307bdf
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 18:51:57 +0800

    count input list

--- display_numbers_no_duplicate.py
+++ display_numbers_no_duplicate.py
@@ -6,4 +6,9 @@ for i in range(10):
     number = int(input("Enter a number: "))
     input_list.append(number) #Adds the number to the list
 
+#Counts how many times each number appears in the list 
+for num in input_list:
+    input_list.count(num) == 1 
+    print(num) #Prints the number if it appears only once
+
 
