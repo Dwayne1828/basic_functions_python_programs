27c37bbe4995ab30510a2da3cd02ea79537c37b0
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:58:04 +0800

    prints not ending zero and five

--- zero_to_hundred_except_ending_zero_five.py
+++ zero_to_hundred_except_ending_zero_five.py
@@ -1,4 +1,8 @@
 
 #Using while loop to print all numbers from 0 to 100 except 5 and 50
-
+count = 0
+while count <= 100:
 #Check if it is divisible by 5 which ends in 5 and 0 
+    if count % 5 != 0:
+        print(count)
+    count += 1
