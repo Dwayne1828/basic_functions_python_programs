eb11de07ad726407bb24be78c10ab82bc2c55803
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 17:11:49 +0800

    num1 less than num2 run

--- prints_between_input_two_numbers.py
+++ prints_between_input_two_numbers.py
@@ -1,7 +1,11 @@
 
 #Asks the user for two numbers
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 #Check whose number is the smallest and biggest 
-
+if num1 < num2:
 #Using for loop that sets the range to what is inputted by the user 
-#Prints the numbers between the two numbers
+    for i in range(num1 + 1, num2):
+        print(i) #Prints the numbers between the two numbers
+
