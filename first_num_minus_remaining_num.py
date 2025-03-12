fdf03cc68571f5f93815bdcb7c4dea410891ce62
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:31:40 +0800

    pseudocode first minus remaining

---
+++ first_num_minus_remaining_num.py
@@ -0,0 +1,10 @@
+
+#Asks the user for 10 numbers using a loop
+
+#Store the first number in a variable and the sum of remaining 
+
+#Solve for the difference of the first number and the sum of the remaining numbers
+
+#Print the difference 
+
+    
