6eb0e52052c28e3ff3a7d280e668e3eb4b9b13ff
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:33:33 +0800

    iterate num and store values

--- first_num_minus_remaining_num.py
+++ first_num_minus_remaining_num.py
@@ -1,7 +1,13 @@
 
 #Asks the user for 10 numbers using a loop
-
+for i in range(10): 
+    num = int(input("Enter a number: "))
+    
 #Store the first number in a variable and the sum of remaining 
+    if i == 0:
+        first_num = num
+    else:
+        sum_remaining += num
 
 #Solve for the difference of the first number and the sum of the remaining numbers
 
