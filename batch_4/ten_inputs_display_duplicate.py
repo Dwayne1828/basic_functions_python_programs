afb62644e0ec2fdd695ed603b8d9bd8f8531a08a
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 23:20:13 +0800

    function to look for dupli and prints

--- batch_4/ten_inputs_display_duplicate.py
+++ batch_4/ten_inputs_display_duplicate.py
@@ -7,4 +7,15 @@ for i in range(10):
     input_value = input("Enter a value: ")
     list_of_inputs.append(input_value)
 
+def display_duplicate(list_of_inputs):
+    #Create a list to store the duplicates
+    duplicates = []
+    
+    for numbers in list_of_inputs:
+        if list_of_inputs.count(numbers) > 1:
+            duplicates.append(numbers) 
+        return duplicates
 
+#Call the function and store the result in a variable
+duplicates = display_duplicate(list_of_inputs)
+print(duplicates)
