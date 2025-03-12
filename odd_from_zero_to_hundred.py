3c37fe5ba894f0ac073f2df4b6f03281fa08fde4
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:51:23 +0800

    prints odd using while

--- odd_from_zero_to_hundred.py
+++ odd_from_zero_to_hundred.py
@@ -1,6 +1,11 @@
 
 #Prints all odd numbers from 0 to 100
 #Using the while loop to check if it is odd and print it
-
+count = 0
+while count <= 100:
+    if count % 2 != 0:
+        print(count)
+    count += 1
+    
 
 
