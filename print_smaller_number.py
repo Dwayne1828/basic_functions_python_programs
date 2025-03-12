aa01ab133002ac8b66426a22705b939a1436c00e
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 15:58:38 +0800

    prints smaller num

--- print_smaller_number.py
+++ print_smaller_number.py
@@ -1,5 +1,11 @@
 
 #Asks the user for two numbers 
-
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 #Compares the two numbers and prints the smaller number
+if num1 < num2:
+    print(num1)
+elif num2 < num1:
+    print(num2)
+    
