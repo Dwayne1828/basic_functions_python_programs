083679e406ca248e88280ee741330541eb97368b
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 18:48:44 +0800

    creates a list and inputs

---
+++ display_numbers_no_duplicate.py
@@ -0,0 +1,9 @@
+
+input_list = [] #Creates an empty list to store the numbers
+
+#Asks the users for 10 numbers 
+for i in range(10):
+    number = int(input("Enter a number: "))
+    input_list.append(number) #Adds the number to the list
+
+
