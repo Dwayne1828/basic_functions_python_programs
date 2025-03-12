575f073455e55e3241bcf049333946f35bb4907b
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 22:15:47 +0800

    function for unique or dupli done

--- ask_till_invalid_unique_and_duplicate.py
+++ ask_till_invalid_unique_and_duplicate.py
@@ -1,8 +1,19 @@
 
+list_of_numbers = [] #Creates an empty list to store the numbers
+
 #Asks til the user enters an invalid input
 while True: 
     try:
-        n = int(input("Enter a number: "))
+        number = int(input("Enter a number: "))
+        list_of_numbers.append(number) #Adds the number
     except ValueError:
         break
-   
+
+def unique_and_duplicate_numbers(list_of_numbers):
+    if list_of_numbers.count(number) == 1:
+        return "Unique"
+    else:
+        return "Duplicate"
+    
+unique_or_duplicate = unique_and_duplicate_numbers(list_of_numbers) 
+print(unique_or_duplicate) 
