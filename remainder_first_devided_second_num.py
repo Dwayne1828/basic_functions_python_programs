ae61b809fce23fb1dea42a5509d9f3c2ef5b87e5
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:24:24 +0800

    remainder output using modulo

--- remainder_first_devided_second_num.py
+++ remainder_first_devided_second_num.py
@@ -1,7 +1,10 @@
 
 #Asks the user for two numbers
-
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
 #Solve for the remainder using modulo division
+remainder = num1 % num2
 
 #Print the remainder of the two numbers
+print(remainder)
