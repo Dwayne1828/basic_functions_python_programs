bd2cd33bff13b8406bcfb0541284779cd956d7ec
Dwayne1828 <dwaynebarrameda093@gmail.com>
2025-03-12 16:07:07 +0800

    prints not equal done

--- print_not_equal_diff_value.py
+++ print_not_equal_diff_value.py
@@ -1,5 +1,8 @@
 
 #Asks the user for two number inputs 
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
 
-#Checks if not equal and prints "not equal" if true
-
+#Checks if not equal and prints "Not equal" if true
+if num1 != num2:
+    print("Not equal")
